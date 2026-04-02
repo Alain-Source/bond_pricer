3d65e63887930108ab1b239fa1717457e92db9e1
Alain <Alainrautenbach@gmail.com>
2026-04-02 10:41:47 +0200

    Cleaned up Convexity method

--- bond.py
+++ bond.py
@@ -71,5 +71,5 @@ class Bond:
         for i in range(1, self.total_num_payments):                                                            
             weighted_present_value += self._present_value(self.coupon_payment, period_yield_rate, i) * i * (i + 1)                         
         weighted_present_value += self._present_value(self.coupon_payment + self.face_value, period_yield_rate, self.total_num_payments) * self.total_num_payments * (self.total_num_payments + 1) 
-        weighted_present_value = ((weighted_present_value / self.price(yield_rate)) / (1 + period_yield_rate) ** 2) / (self.frequency ** 2)
+        weighted_present_value / (self.price(yield_rate) * (1 + period_yield_rate) ** 2 * self.frequency ** 2)
         return weighted_present_value
