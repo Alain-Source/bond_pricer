788777a93f7a72eb33d5b4e733a0ea5a49324cd9
Alain <Alainrautenbach@gmail.com>
2026-04-02 09:19:34 +0200

    Refactor: extract _present_value helper, move derived attributes to init, fix macaulay_duration bug

--- bond.py
+++ bond.py
@@ -1,35 +1,39 @@
 class Bond:
 
-    def __init__(self, face_value, coupon_rate, maturity, frequency) -> None:   # initialise the c
+    def __init__(self, face_value, coupon_rate, maturity, frequency) -> None:   # initialise the class
         self.face_value = face_value
         self.coupon_rate = coupon_rate      # Percentage of face value which determines the coupon payment
         self.maturity = maturity            # Number of years to bond maturity
         self.frequency = frequency          # Number of bond payments per year
 
-    def price(self, yield_rate):        
+        # Compute coupon_payment & total_num_payments once at object initialization
+
         # Face value multiplied by coupon rate for total yearly coupon payment. 
         # Frequency adjusts payment to account for number of payments in a year
-        coupon_payment = self.face_value * self.coupon_rate / self.frequency
-
-        # Maturity is the number of years to bond maturity.
+        self.coupon_payment = self.face_value * self.coupon_rate / self.frequency   
         # Frequency adjusts for number of payments per year.
-        total_num_payments = self.maturity * self.frequency 
+        self.total_num_payments =  self.maturity * self.frequency                   
 
+    def _present_value(self, cash_flow, period_yield_rate, period):          # Internal class helper method
+        return cash_flow/(1 + period_yield_rate) ** period                   # Per period present_value calculation
+
+    
+    def price(self, yield_rate):        
         # Yield per coupon payment
         period_yield_rate = yield_rate / self.frequency  
                                                                                
-        bond_price = 0                                                                                    # Bond Price Formula | PV = SUM_1_to_n(PMT / (1 + r)^n) + FV/(1 + r)^n , n = {1,2,..,n} 
-        for i in range(1, total_num_payments):                                                            # Range from 1 to n-1 as the final payment will include interest payment & face value.
-            bond_price += coupon_payment/(1 + period_yield_rate) ** i                                     # Coupon payment per period (i) added to the total coupon payment
-        bond_price += (coupon_payment + self.face_value)/(1 + period_yield_rate) ** total_num_payments    # Final payment includes coupon payment & face value
+        bond_price = 0                                                                                                         # Bond Price Formula | PV = SUM_1_to_n(PMT / (1 + r)^n) + FV/(1 + r)^n , n = {1,2,..,n} 
+        for i in range(1, self.total_num_payments):                                                                            # Range from 1 to n-1 as the final payment will include interest payment & face value.
+            bond_price += self._present_value(self.coupon_payment, period_yield_rate, i)                                       # Coupon payment per period (i) added to the total coupon payment
+        bond_price += self._present_value(self.coupon_payment + self.face_value, period_yield_rate, self.total_num_payments)   # Final payment includes coupon payment & face value
         return bond_price
 
-    def ytm(self, market_price, accuracy = 0.0001):    # implement a bescetion guess method
+    def ytm(self, market_price, accuracy = 0.0001):                             # implement bisection guess method
         test_yield_lower_bound = 0.0001
         test_yield_upper_bound = 1
         
-        midpoint = (test_yield_upper_bound + test_yield_lower_bound) / 2    # Guess what the yield is by testing the midpoint between the two values
-        test_price = self.price(midpoint)                                   # Calculate what the bond price is at that midpoint yield
+        midpoint = (test_yield_upper_bound + test_yield_lower_bound) / 2        # Guess what the yield is by testing the midpoint between the two values
+        test_price = self.price(midpoint)                                       # Calculate what the bond price is at that midpoint yield
 
         # Check whether the price resulting from the test yield's bounds equal to 
         # or within the specified accuracy of the market price.
@@ -41,17 +45,14 @@ class Bond:
             
             midpoint = (test_yield_upper_bound + test_yield_lower_bound) / 2    # Guess what the yield is by testing the midpoint between the two values
             test_price = self.price(midpoint)                                   # Calculate what the bond price is at that midpoint yeild
-
         return midpoint
 
-    # TODO: Add explanation of what macaulay duration is & how it relates to sensitivity with interest rate changes
-    def macaulay_duration(self, yield_rate):    
-        coupon_payment = self.face_value * self.coupon_rate / self.frequency
-        total_num_payments = self.maturity * self.frequency 
+    # Macaulay Duration is the weighted average time to receive the bond's cash flows.
+    def macaulay_duration(self, yield_rate):   
         period_yield_rate = yield_rate / self.frequency  
                                                                                
-        weighted_present_value = 0                                                                                     
-        for i in range(1, total_num_payments):                                                            
-            weighted_present_value += (coupon_payment/(1 + period_yield_rate) ** i) * i / self.frequency                                  
-        weighted_present_value += ((coupon_payment + self.face_value)/(1 + period_yield_rate) ** total_num_payments) * total_num_payments
+        weighted_present_value = 0                                                                                            # Weight each period's payment by it's payment number                                                                    
+        for i in range(1, self.total_num_payments):                                                            
+            weighted_present_value += self._present_value(self.coupon_payment, period_yield_rate, i) * i / self.frequency     # Weight by (period / frequency of payments per period) to determine payment number                      
+        weighted_present_value += self._present_value(self.coupon_payment + self.face_value, period_yield_rate, self.total_num_payments) * self.total_num_payments/ self.frequency 
         return weighted_present_value / self.price(yield_rate)
