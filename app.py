f04255cbd284d408d4864032540028c93bdf41bd
Alain <Alainrautenbach@gmail.com>
2026-04-03 12:02:49 +0200

    Completed Yield Analysis & Risk Measure feature

--- app.py
+++ app.py
@@ -5,32 +5,151 @@ from bond import Bond
 # App Title & Sub-title
 st.title("Bond Price Calculator")
 st.markdown("""An interactive learning tool for exploring fixed-income fundamentals.
-Calculate bond prices, yields, and risk measures. 
-Visualise how bond prices respond to changes in yield rates.""", unsafe_allow_html=True)
+<br>Calculate bond prices, yields, and risk measures. 
+Visualise bond prices responding to changes in yield rates.""", unsafe_allow_html=True)
 
 # Setup Tabs
 tab1, tab2 = st.tabs(["Calculator", "Visualizer"])
 
 # Calculator Tab
 with tab1:
-    st.write("Bond Contract")
-    col1, col2 = st.columns(2)
-    
-    with col1:
-        face_value = st.number_input("Face Value (R)", value=1000)
-        coupon_rate = st.number_input("Coupon Rate", value=0.05)
-        maturity = st.number_input("Maturity (years)", value=10)
-
-    with col2:
-        frequency = st.number_input("Frequency", value=1)
-        yield_rate = st.number_input("Yield Rate", value=0.05)
-
-    if st.button("Calculate"):
+    with st.container(border=True):
+        st.subheader("Bond Contract")
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            face_value = st.number_input("Face Value (R)", value=1000)
+            coupon_rate = st.number_input("Coupon Rate (%)", value=0.05)
+            
+        with col2:
+            maturity = st.number_input("Maturity (years)", value=10)
+            frequency = st.number_input("Frequency", value=1)
+        
         bond = Bond(face_value, coupon_rate, maturity, frequency)
-        st.write(f"Bond Price: R {bond.price(yield_rate):.2f}")
 
-# Relationship Visuali  zation Tab
+    with st.container(border=True):
+        st.subheader("Yield Analysis")
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            with st.container(border=True):        
+                yield_rate = st.slider("Select a yield rate", min_value=0.0, max_value=1.0, value=0.05, key="analysis_yield")
+                bond_price = bond.price(yield_rate)
+
+                st.markdown("<br>", unsafe_allow_html=True)
+                st.markdown("""
+                <style>
+                    [data-testid="stMetricValue"] {
+                        color: #69F0AE;
+                        font-weight: bold;
+                    }
+                </style>
+                """, unsafe_allow_html=True)
+                st.metric(label="Bond Price", value=f"R {bond_price:.2f}")
+
+
+        with col2:
+            with st.container(border=True):
+                max_market_price = bond.price(0.0001)
+                min_market_price = bond.price(1.0)
+                market_price = st.slider("Select the bond's market price", min_value=min_market_price, max_value=max_market_price, value=float(face_value), step=1.0, key="analysis_price")
+                
+                yield_to_maturity = bond.ytm(market_price)
+
+                st.markdown("<br>", unsafe_allow_html=True)
+                st.markdown("""
+                <style>
+                    [data-testid="stMetricValue"] {
+                        color: #69F0AE;
+                        font-weight: bold;
+                    }
+                </style>    
+                """, unsafe_allow_html=True)
+                st.metric(label="Yield to Maturity", value=f"{yield_to_maturity:.2f} %")
+    
+    with st.container(border=True):
+        st.subheader("Risk Measures")
+        
+        yield_rate = st.slider("Select a yield rate", min_value=0.0, max_value=1.0, value=0.05, key="risk_measures_yield")
+        macaulay_duration = bond.macaulay_duration(yield_rate)
+        modified_duration = bond.modified_duration(yield_rate)
+        convexity = bond.convexity(yield_rate)
+        
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            with st.container(border=True):        
+                st.markdown("<br>", unsafe_allow_html=True)
+                st.markdown("""
+                <style>
+                    [data-testid="stMetricValue"] {
+                        color: #69F0AE;
+                        font-weight: bold;
+                    }
+                </style>
+                """, unsafe_allow_html=True)
+                st.metric(label="Macaulay Duration", value=f"{macaulay_duration:.2f} years")
+
+
+        with col2:
+            with st.container(border=True):        
+                st.markdown("<br>", unsafe_allow_html=True)
+                st.markdown("""
+                <style>
+                    [data-testid="stMetricValue"] {
+                        color: #69F0AE;
+                        font-weight: bold;
+                    }
+                </style>
+                """, unsafe_allow_html=True)
+                st.metric(label="Modified Duration", value=f"{modified_duration:.2f} %")
+
+        with col3:
+            with st.container(border=True):        
+                st.markdown("<br>", unsafe_allow_html=True)
+                st.markdown("""
+                <style>
+                    [data-testid="stMetricValue"] {
+                        color: #69F0AE;
+                        font-weight: bold;
+                    }
+                </style>
+                """, unsafe_allow_html=True)
+                st.metric(label="Convexity", value=f"{convexity:.2f}")
+
+
+# Relationship Visualization Tab
 with tab2:
-    st.write("In Development")
+    with st.container(border=True):
+        st.subheader("Visualiser")
 
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            with st.container(border=True):
+                st.subheader("Bond 1")    
+                face_value_1 = st.number_input("Face Value (R)", value=1000, key="vis_bond_1_fc")
+                coupon_rate_1 = st.number_input("Coupon Rate (%)", value=0.05, key="vis_bond_1_cr")
+                maturity_1 = st.number_input("Maturity (years)", value=10, key="vis_bond_1_mat")
+                frequency_1 = st.number_input("Frequency", value=1, key="vis_bond_1_frq")
+                bond_1_enabled = st.checkbox("Enable", value=True, key="bond_1_checkbox")
 
+        with col2:
+            with st.container(border=True):
+                st.subheader("Bond 2") 
+                face_value_2 = st.number_input("Face Value (R)", value=1000, key="vis_bond_2_fc")
+                coupon_rate_2 = st.number_input("Coupon Rate (%)", value=0.05, key="vis_bond_2_cr")
+                maturity_2 = st.number_input("Maturity (years)", value=10, key="vis_bond_2_mat")
+                frequency_2 = st.number_input("Frequency", value=1, key="vis_bond_2_frq")
+                bond_2_enabled = st.checkbox("Enable", value=True, key="bond_2_checkbox")
+
+        with col3:
+            with st.container(border=True):
+                st.subheader("Bond 3") 
+                face_value_3 = st.number_input("Face Value (R)", value=1000, key="vis_bond_3_fc")
+                coupon_rate_3 = st.number_input("Coupon Rate (%)", value=0.05, key="vis_bond_3_cr")
+                maturity_3 = st.number_input("Maturity (years)", value=10, key="vis_bond_3_mat")
+                frequency_3 = st.number_input("Frequency", value=1, key="vis_bond_3_frq")
+                bond_3_enabled = st.checkbox("Enable", value=True, key="bond_3_checkbox")
+            
+            bond_1 = Bond(face_value_1, coupon_rate_1, maturity_1, frequency_1)
+            bond_2 = Bond(face_value_2, coupon_rate_2, maturity_2, frequency_2)
+            bond_3 = Bond(face_value_3, coupon_rate_3, maturity_3, frequency_3)
--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def main():  # Main function not required in Python (like in C). Used for organi
     print(f"Convexity:          {test_bond.convexity(0.05)}")
 
     # Testing Bond methods
-    test_bond_2 = Bond(face_value=1000, coupon_rate=0.0, maturity=10, frequency=1)
+    test_bond_2 = Bond(face_value=1000, coupon_rate=0.05, maturity=10, frequency=1)
     print("\nBOND 2")
     print(f"Price:              {test_bond_2.price(0.05)}")
     print(f"Macaulay Duration:  {test_bond_2.macaulay_duration(0.05)}")
     print(f"Modified Duration:  {test_bond_2.modified_duration(0.05)}")
     print(f"Convexity:          {test_bond_2.convexity(0.05)}")
-
+    
     # Investigate the relationship between yeild rates & bond prices
     yield_range = np.linspace(0.01, 0.15, 100)
     yield_range_prices = []
